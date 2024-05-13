c605f661b6dfefe200f28fe37096c9c0ed563adc
Todd Viegut <todd.viegut@gmail.com>
2024-05-12 19:24:19 -0500

    #108

--- CIMToolPlugin/native/linux-x86/README.txt
+++
@@ -1,2 +0,0 @@
-Currently CIMTool is not built/supported on linux due to lack of resources to contribute & test on that OS. 
-The folder is retained in the event that it is supported in the future.
--- CIMToolPlugin/src/au/com/langdale/cimtoole/CIMToolPlugin.java
+++ CIMToolPlugin/src/au/com/langdale/cimtoole/CIMToolPlugin.java
@@ -4,14 +4,19 @@
  */
 package au.com.langdale.cimtoole;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.Bundle;
@@ -28,6 +33,9 @@ import au.com.langdale.ui.util.NodeTraits;
  * model cache.
  */
 public class CIMToolPlugin extends AbstractUIPlugin {
+
+	private static final String OS_ARCH = "os.arch";
+	private static final String OS_NAME = "os.name";
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "au.com.langdale.cimtoole";
@@ -55,61 +63,90 @@ public class CIMToolPlugin extends AbstractUIPlugin {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		setJNALibraryPath();
-		//
 		super.start(context);
+		loadFirebirdEmbeddedLibraries();
 		IconCache.setIcons(new CIMToolIconCache(context.getBundle(), "/icons/"));
 		cache = new Cache();
 		settings = new Settings();
 		plugin = this;
 	}
+	
+	private void loadFirebirdEmbeddedLibraries() {
+		try {
+			// Build the full path to the DLLs
+			String platform = getPlatformFolder(); // Determine the platform-specific subfolder
+			IPath stateLocation = getStateLocation(); 
+			IPath stateLocationNativePlatformPath = stateLocation.append("native").append(platform).append("firebird");
+			File stateLocationNativePlatformDir = stateLocationNativePlatformPath.toFile();
+			
+			File fbClientLib = new File(stateLocationNativePlatformDir, "fbembed.dll");
 
+			if (!fbClientLib.exists()) {
+				// Locate the ZIP file within the bundle...
+				Bundle cimtooleBundle = Platform.getBundle(CIMToolPlugin.PLUGIN_ID);
+				URL fbClientLibURL = cimtooleBundle.getEntry("native/Firebird_" + platform + ".zip");
+				URL fbClientLibFileURL = FileLocator.toFileURL(fbClientLibURL);
+				
+				// Create input stream to the ZIP file in the bundle to unzip to the file system.
+				InputStream zipFileInputStream = fbClientLibFileURL.openStream();
+				unzip(zipFileInputStream, stateLocationNativePlatformDir.getAbsolutePath());
+			}
+			
+			// Finally set the 
+			System.setProperty("jna.library.path", stateLocationNativePlatformDir.getAbsolutePath());
+			System.err.println(System.getProperty("jna.library.path"));
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void unzip(InputStream zipFileInputStream, String destDirectory) throws IOException {
+		File destDir = new File(destDirectory);
+		if (!destDir.exists()) {
+			destDir.mkdirs();
+		}
+		ZipInputStream zipIn = new ZipInputStream(zipFileInputStream);
+		ZipEntry entry = zipIn.getNextEntry();
+		while (entry != null) {
+			String filePath = destDirectory + File.separatorChar + entry.getName();
+			if (!entry.isDirectory()) {
+				extractFile(zipIn, filePath);
+			} else {
+				File dir = new File(filePath);
+				dir.mkdir();
+			}
+			zipIn.closeEntry();
+			entry = zipIn.getNextEntry();
+		}
+		zipIn.close();
+	}
+
+	private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+		byte[] bytesIn = new byte[4096];
+		int read;
+		while ((read = zipIn.read(bytesIn)) != -1) {
+			bos.write(bytesIn, 0, read);
+		}
+		bos.close();
+	}
+	
 	private String getPlatformFolder() {
-		String os = System.getProperty("os.name").toLowerCase();
-		String arch = System.getProperty("os.arch").toLowerCase();
+		String os = System.getProperty(OS_NAME).toLowerCase();
+		String arch = System.getProperty(OS_ARCH).toLowerCase();
 
 		if (os.contains("win")) {
-			return arch.contains("64") ? "win32-x86-64" : "win32-x86";
+			return arch.contains("64") ? "win32-x86_64" : "win32-x86";
 		} else if (os.contains("linux")) {
-			return arch.contains("64") ? "linux-x86-64" : "linux-x86";
+			return arch.contains("64") ? "linux-x86_64" : "linux-x86";
 		} else if (os.contains("mac")) {
-			return "macosx-x86-64";
+			return "macosx-x86_64";
 		} else {
 			throw new IllegalStateException("Unsupported platform: " + os);
 		}
 	}
-
-	private void setJNALibraryPath() throws IOException {
-		if (Platform.isRunning()) {
-			Location configLocation = Platform.getConfigurationLocation();
-			URL dataArea = configLocation.getDataArea(CIMToolPlugin.PLUGIN_ID);
-
-			File dataAreaDir = new File(dataArea.getPath());
-			dataAreaDir = new File(dataAreaDir.getPath());
-
-
-			// Specify the path to the directory containing the native libraries
-			// String libraryPath = "C:\\Program Files\\Sparx Systems\\EA\\Firebird";
-			String libraryPath = "C:\\CIMug-GIT-REPOS\\CIMTool\\CIMToolPlugin\\native\\" + getPlatformFolder();
-			// Set the jna.library.path system property
-			System.setProperty("jna.library.path", libraryPath);
-
-		} else {
-			// Determine the platform-specific subfolder
-			String platform = getPlatformFolder();
 	
-			// Build the full path to the DLLs
-			IPath stateLocation = getStateLocation();
-			IPath nativePath = stateLocation.append("native").append(platform);
-			File nativeDir = nativePath.toFile();
-	
-			// Add the native directory to the system property for loading libraries
-			System.setProperty("jna.library.path", nativeDir.getAbsolutePath());
-		}
-
-		System.err.println(System.getProperty("jna.library.path"));
-	}
-
 	private static class CIMToolIconCache extends GeneralIconCache {
 		public CIMToolIconCache(Bundle bundleToUse, String prefixPath) {
 			super(bundleToUse, prefixPath);
